a32a1fda56a2359b3c24df0412c9e4af4c02ee7e
Ayolela Vena <avianrossi@gmail.com>
2026-09-09 22:41:45 +0200

    Remove font links from router.tsx
    Removed font preconnect and stylesheet links from router.

--- src/router.tsx
+++ src/router.tsx
@@ -2,12 +2,6 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@400;500;600&family=Space+Mono&display=swap"
-  rel="stylesheet"
-/>
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
